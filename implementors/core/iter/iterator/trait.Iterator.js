d273187906b936cdc41725b5c9f2dafc49fa418d
ghost <ghost@konpa.ku>
2017-10-25 19:29:01 +0000

    testtag - b04262dc9da1c6066ab022e4511d3136858ae36c

--- implementors/core/iter/iterator/trait.Iterator.js
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -1,4 +1,5 @@
 (function() {var implementors = {};
+implementors["nvapi"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"nvapi/struct.VfpMaskIter.html\" title=\"struct nvapi::VfpMaskIter\">VfpMaskIter</a>&lt;'a&gt;",];
 implementors["nvapi_sys"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"nvapi_sys/gpu/power/private/struct.PerfFlags.html\" title=\"struct nvapi_sys::gpu::power::private::PerfFlags\">PerfFlags</a>",];
 
             if (window.register_implementors) {
---
+++ implementors/core/iter/traits/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["nvapi"] = ["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"nvapi/struct.VfpMask.html\" title=\"struct nvapi::VfpMask\">VfpMask</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
