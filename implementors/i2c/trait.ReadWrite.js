ecc943edea2733db9699aa5f98ff0ce9ac41588c
ghost <ghost@konpa.ku>
2018-03-28 19:38:00 +0000

    0.1.0 - 29e6cde18789938aa8d9d646166d7ee886851e27

---
+++ implementors/i2c/trait.ReadWrite.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["nvapi"] = ["impl <a class=\"trait\" href=\"http://arcnmx.github.io/i2c-rs/i2c/trait.ReadWrite.html\" title=\"trait i2c::ReadWrite\">ReadWrite</a> for <a class=\"struct\" href=\"nvapi/struct.I2c.html\" title=\"struct nvapi::I2c\">I2c</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/i2c/trait.Smbus.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["nvapi"] = ["impl <a class=\"trait\" href=\"http://arcnmx.github.io/i2c-rs/i2c/trait.Smbus.html\" title=\"trait i2c::Smbus\">Smbus</a> for <a class=\"struct\" href=\"nvapi/struct.I2c.html\" title=\"struct nvapi::I2c\">I2c</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
