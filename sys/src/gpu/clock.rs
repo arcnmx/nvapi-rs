5e29a6d39bbf19ff0b1c56525e7f6a7fafaeae69
Мурза Лейн <murza8965@gmail.com>
2023-02-01 17:50:58 +0500

    Two new unknown limiters

--- sys/src/gpu/clock.rs
+++ sys/src/gpu/clock.rs
@@ -249,7 +249,9 @@ pub mod private {
             NV_PERF_CLIENT_LIMIT_ID_GPU / Gpu = 0,
             NV_PERF_CLIENT_LIMIT_ID_GPU_UNKNOWN / GpuUnknown = 1,
             NV_PERF_CLIENT_LIMIT_ID_MEMORY / Memory = 2,
-            NV_PERF_CLIENT_LIMIT_ID_MEMORY_UNKOWN / MemoryUnknown = 3,
+            NV_PERF_CLIENT_LIMIT_ID_MEMORY_UNKNOWN / MemoryUnknown = 3,
+            NV_PERF_CLIENT_LIMIT_ID_UNKNOWN_4 / Unknown_4 = 4,
+            NV_PERF_CLIENT_LIMIT_ID_UNKNOWN_5 / Unknown_5 = 5,
             NV_PERF_CLIENT_LIMIT_ID_VOLTAGE / Voltage = 6,
         }
     }
