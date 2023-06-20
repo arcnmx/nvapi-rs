c401618cf1e0a0b20f2330f470b9bb1201ac1fa8
arcnmx <git@git.arcn.mx>
2023-06-20 15:03:25 -0700

    refactor: update test to rust 2021

--- tests/api_calls_return.rs
+++ tests/api_calls_return.rs
@@ -1,19 +1,18 @@
-// Tests in this file call apis an simply expect them to return without causing 
+// Tests in this file call apis and simply expect them to return without causing
 // segfaults, etc.
 
 // Not actually testing results. Just calling the api.
 #![allow(unused_must_use)]
 
-extern crate nvapi;
-
+use nvapi::{PhysicalGpu, ConnectedIdsFlags};
 
 #[test]
 fn physicalgpu_display_ids_connected() {
     if let Ok(_) = nvapi::initialize() {
-        if let Ok(gpus) = nvapi::PhysicalGpu::enumerate() {
+        if let Ok(gpus) = PhysicalGpu::enumerate() {
             for gpu in gpus {
                 // Bug: if there are zero connected displays this may crash.
-                gpu.display_ids_connected(nvapi::ConnectedIdsFlags::empty());
+                gpu.display_ids_connected(ConnectedIdsFlags::empty());
             }
         }
     }
@@ -23,7 +22,7 @@ fn physicalgpu_display_ids_connected() {
 #[test]
 fn physicalgpu_display_ids_all() {
     if let Ok(_) = nvapi::initialize() {
-        if let Ok(gpus) = nvapi::PhysicalGpu::enumerate() {
+        if let Ok(gpus) = PhysicalGpu::enumerate() {
             for gpu in gpus {
                 // Bug: if there are zero connected displays this may crash.
                 gpu.display_ids_all();
